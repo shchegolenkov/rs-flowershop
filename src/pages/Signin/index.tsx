d772a6ddeab4ed41e30752a00554252d9572db9f
Mialiokhin <a.mialiokhin@gmail.com>
2023-08-13 16:17:21 +0300

    feat (rs-flowershop-20,21) add singin header to Signin layout

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import RootLayout from '../../layouts/index';
+import clsx from 'clsx';
+import { Typography } from '../../components/ui/Typography';
+import s from './Singin.module.scss';
 
 const Signin: React.FC = () => {
-  return <RootLayout></RootLayout>;
+  return (
+    <RootLayout>
+      <div className={s.main}>
+        <div className={clsx(s.elements__flow, s.signin__header, s.signin__header_flow)}>
+          <div className={clsx(s.form__element, s.signin__header_element, s.header__img)}></div>
+          <div className={clsx(s.form__element, s.signin__header_element, s.header__message)}>
+            <div className={s.header__message_container}>
+              <Typography variant="h2" className={s.welcome}>
+                Welcome! Create
+                <br /> your account now
+              </Typography>
+            </div>
+          </div>
+        </div>
+      </div>
+    </RootLayout>
+  );
 };
 export default Signin;
