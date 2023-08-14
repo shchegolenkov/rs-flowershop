8a695323fd91e935b217b28eedaa421c96c92be3
Mialiokhin <a.mialiokhin@gmail.com>
2023-08-14 20:31:41 +0300

    fix (rs-flowershop-20,21) add interface to EmailInput

--- src/components/UI/EmailInput/index.tsx
+++ src/components/UI/EmailInput/index.tsx
@@ -3,8 +3,16 @@ import TextField from '@mui/material/TextField';
 import { ListItemIcon, ListItemText } from '@mui/material';
 import ErrorIcon from '@mui/icons-material/Error';
 import React from 'react';
+import { FieldError, FieldValues, UseFormRegister } from 'react-hook-form';
 
-const EmailInput = ({ register, errors }) => {
+interface EmailInputProps {
+  register: UseFormRegister<FieldValues>;
+  errors: {
+    email?: FieldError;
+  };
+}
+
+const EmailInput: React.FC<EmailInputProps> = ({ register, errors }) => {
   return (
     <div className={s.form__field_size}>
       <TextField
