679db78cf3df44ec43a14b28a82a77f911abe8bf
Mialiokhin <a.mialiokhin@gmail.com>
2023-08-15 18:34:12 +0300

    fix (rs-flowershop-20,21) right path to component

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -8,7 +8,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 
 import RootLayout from '../../layouts/index';
-import { Typography } from '../../components/ui/Typography';
+import { Typography } from '../../components/UI/Typography';
 import FormTheme from '../../themes/FormTheme';
 import { validateEmail } from '../../utils/validators';
 import { CustomerData } from '../../types/types';
