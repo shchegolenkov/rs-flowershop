f42296380c9e2cfac37d64587e67da5d143f67db
Mialiokhin <a.mialiokhin@gmail.com>
2023-08-18 18:55:08 +0300

    feat (rs-flowershop-20,21) add SimpleInput component

---
+++ src/components/UI/FormFields/SimpleInput/index.tsx
@@ -0,0 +1,48 @@
+import s from './SimpleInput.module.scss';
+import TextField from '@mui/material/TextField';
+import ErrorIcon from '@mui/icons-material/Error';
+import React from 'react';
+import { FieldError, FieldErrors, UseFormRegister } from 'react-hook-form';
+import { CustomerData } from '../../../../types/types';
+
+interface StreetInputProps {
+  register: UseFormRegister<CustomerData>;
+  name: keyof CustomerData;
+  err: FieldError | undefined;
+  errMessage: string | undefined;
+  errors: FieldErrors<CustomerData>;
+  label: string;
+  id: string;
+}
+
+const SimpleInput: React.FC<StreetInputProps> = ({
+  register,
+  err,
+  errMessage,
+  name,
+  label,
+  id,
+}) => {
+  return (
+    <div className={s.form__field_size}>
+      <TextField
+        fullWidth
+        label={label}
+        error={!!err}
+        id={id}
+        {...register(name)}
+        helperText={
+          err ? (
+            <span className={s.err__message}>
+              <ErrorIcon color="error" /> {errMessage}
+            </span>
+          ) : (
+            ''
+          )
+        }
+      />
+    </div>
+  );
+};
+
+export default SimpleInput;
