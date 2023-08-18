39e6266ac83aff40aa8aa8d2ea418027d3ec1f9d
Mialiokhin <a.mialiokhin@gmail.com>
2023-08-18 18:53:44 +0300

    feat (rs-flowershop-20,21) add types shippingStreet, shippingCity, del addresses in CustomerData

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,15 +1,9 @@
-export interface Address {
-  street: string;
-  city: string;
-  postalCode: string;
-  country: string;
-}
-
 export interface CustomerData {
   email: string;
   password: string;
   firstName: string;
   lastName: string;
   birthDate: Date | null;
-  addresses: Address[];
+  shippingStreet: string;
+  shippingCity: string;
 }
