83eebe3caa5fb8e1c0ff3645a28727e7e045b399
shchegolenkov <denis.amorales@ya.ru>
2023-08-21 23:27:50 +0300

    chore (rs-flowershop-38) setup jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,17 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  roots: ['./src'],
+  setupFilesAfterEnv: ['<rootDir>/jest.config.js'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/mocks/fileMock.ts',
+    '\\.svg\\?url$': '<rootDir>/src/mocks/fileMock.ts',
+    '\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testMatch: ['**/?(*.)(spec|test).[jt]s?(x)'],
+  testEnvironment: 'jsdom',
 };
