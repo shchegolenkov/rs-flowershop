500421416780ff25c8ba1effcf773672598db25b
shchegolenkov <denis.amorales@ya.ru>
2023-08-24 21:58:12 +0300

    feat (rs-flowershop-18) response message typing

--- src/app/slices/message.ts
+++ src/app/slices/message.ts
@@ -1,12 +1,18 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-const initialState = {};
+interface MessageState {
+  message: string;
+}
+
+const initialState: MessageState = {
+  message: '',
+};
 
 const messageSlice = createSlice({
   name: 'message',
   initialState,
   reducers: {
-    setMessage: (state: string, action: PayloadAction<string>) => {
+    setMessage: (state, action: PayloadAction<string>) => {
       return { message: action.payload };
     },
     clearMessage: () => {
