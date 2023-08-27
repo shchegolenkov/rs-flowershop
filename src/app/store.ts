e959bb978d7a03f87fdeb2a8731c0fbab2139ea3
Mialiokhin <a.mialiokhin@gmail.com>
2023-08-27 23:40:26 +0300

    feat (rs-flowershop-58) add profile for store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/auth';
 import messageReducer from './slices/message';
+import profileReducer from './slices/profile';
 
 const reducer = {
   auth: authReducer,
   message: messageReducer,
+  profile: profileReducer,
 };
 
 export const store = configureStore({
