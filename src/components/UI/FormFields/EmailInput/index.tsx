95e275a554afcfdd38baef94525be35c2327a528
Mialiokhin <a.mialiokhin@gmail.com>
2023-08-27 23:38:45 +0300

    feat (rs-flowershop-58) adaptive form fields for profile page

--- src/components/UI/FormFields/EmailInput/index.tsx
+++ src/components/UI/FormFields/EmailInput/index.tsx
@@ -4,33 +4,55 @@ import ErrorIcon from '@mui/icons-material/Error';
 import React from 'react';
 import { FieldError, UseFormRegister } from 'react-hook-form';
 import { CustomerData } from '../../../../types/types';
+import EditIco from '../../../../assets/svg/edit.svg';
+import EditIcoActive from '../../../../assets/svg/editActive.svg';
 
 interface EmailInputProps {
   register: UseFormRegister<CustomerData>;
   errors: {
     email?: FieldError;
   };
+  defaultValue?: string;
+  isEditField?: boolean;
+  isDisabled?: boolean;
+  switchEditModeField?: () => void;
 }
 
-const EmailInput: React.FC<EmailInputProps> = ({ register, errors }) => {
+const EmailInput: React.FC<EmailInputProps> = ({
+  register,
+  errors,
+  defaultValue,
+  isEditField,
+  isDisabled,
+  switchEditModeField,
+}) => {
   return (
-    <div className={s.form__field_size}>
-      <TextField
-        fullWidth
-        label="Email *"
-        error={!!errors.email}
-        id="email-input"
-        {...register('email')}
-        helperText={
-          errors.email ? (
-            <span className={s.err__message}>
-              <ErrorIcon color="error" /> {errors.email.message}
-            </span>
-          ) : (
-            ''
-          )
-        }
-      />
+    <div className={s.edit__field_container}>
+      <div className={s.form__field_size}>
+        <TextField
+          disabled={isEditField ? isDisabled : false}
+          defaultValue={defaultValue}
+          fullWidth
+          label="Email *"
+          error={!!errors.email}
+          id="email-input"
+          {...register('email')}
+          helperText={
+            errors.email ? (
+              <span className={s.err__message}>
+                <ErrorIcon color="error" /> {errors.email.message}
+              </span>
+            ) : (
+              ''
+            )
+          }
+        />
+      </div>
+      {isEditField ? (
+        <button type="button" onClick={switchEditModeField}>
+          {isDisabled ? <EditIco /> : <EditIcoActive />}
+        </button>
+      ) : null}
     </div>
   );
 };
--- src/components/UI/FormFields/SimpleInput/index.tsx
+++ src/components/UI/FormFields/SimpleInput/index.tsx
@@ -4,6 +4,9 @@ import ErrorIcon from '@mui/icons-material/Error';
 import React from 'react';
 import { FieldError, FieldErrors, UseFormRegister } from 'react-hook-form';
 import { CustomerData } from '../../../../types/types';
+import EditIco from '../../../../assets/svg/edit.svg';
+import EditIcoActive from '../../../../assets/svg/editActive.svg';
+import EditIcoErr from '../../../../assets/svg/editErr.svg';
 
 interface SimpleInputProps {
   register: UseFormRegister<CustomerData>;
@@ -13,6 +16,10 @@ interface SimpleInputProps {
   errors: FieldErrors<CustomerData>;
   label: string;
   id: string;
+  defaultValue?: string;
+  isEditField?: boolean;
+  isDisabled?: boolean;
+  switchEditModeField?: () => void;
 }
 
 const SimpleInput: React.FC<SimpleInputProps> = ({
@@ -22,25 +29,46 @@ const SimpleInput: React.FC<SimpleInputProps> = ({
   name,
   label,
   id,
+  defaultValue,
+  isEditField,
+  isDisabled,
+  switchEditModeField,
 }) => {
   return (
-    <div className={s.form__field_size}>
-      <TextField
-        fullWidth
-        label={label}
-        error={!!err}
-        id={id}
-        {...register(name)}
-        helperText={
-          err ? (
-            <span className={s.err__message}>
-              <ErrorIcon color="error" /> {errMessage}
-            </span>
+    <div className={s.edit__field_container}>
+      <div className={s.form__field_size}>
+        <TextField
+          disabled={isEditField ? isDisabled : false}
+          defaultValue={defaultValue}
+          fullWidth
+          label={label}
+          error={!!err}
+          id={id}
+          {...register(name)}
+          helperText={
+            err ? (
+              <span className={s.err__message}>
+                <ErrorIcon color="error" /> {errMessage}
+              </span>
+            ) : (
+              ''
+            )
+          }
+        />
+      </div>
+      {isEditField ? (
+        <button type="button" onClick={!errMessage ? switchEditModeField : undefined}>
+          {isDisabled ? (
+            <EditIco />
+          ) : !errMessage ? (
+            <EditIcoActive />
           ) : (
-            ''
-          )
-        }
-      />
+            <div className={s.err_edit_btn}>
+              <EditIcoErr />
+            </div>
+          )}
+        </button>
+      ) : null}
     </div>
   );
 };
