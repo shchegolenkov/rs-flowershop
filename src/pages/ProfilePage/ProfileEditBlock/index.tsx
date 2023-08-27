9641ce5d48447689e46621f97b31a789dc296796
Mialiokhin <a.mialiokhin@gmail.com>
2023-08-27 23:44:18 +0300

    feat (rs-flowershop-58) add edit mode for personal info and email

---
+++ src/pages/ProfilePage/ProfileEditBlock/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import clsx from 'clsx';
+import s from '../ProfilePage.module.scss';
+import Alert from '@mui/material/Alert';
+import Button from '../../../components/UI/Button';
+
+interface ProfileEditBlockProps {
+  onClickSubmit: () => void;
+  onClickCancel: () => void;
+  formError: boolean;
+  isSuccess: boolean;
+  message: string | null;
+}
+
+const ProfileEditBlock: React.FC<ProfileEditBlockProps> = ({
+  onClickSubmit,
+  onClickCancel,
+  formError,
+  isSuccess,
+  message,
+}) => {
+  return (
+    <div>
+      <div className={clsx(s.save_edit_container)}>
+        <Button type="submit" variant="success" onClick={onClickSubmit}>
+          SAVE EDIT
+        </Button>
+        <Button type="button" variant="red" onClick={onClickCancel}>
+          Cancel edit
+        </Button>
+      </div>
+      <div className={s.alert_position}>
+        {formError ? (
+          <Alert variant="outlined" severity="error">
+            Something went wrong - please check the form!
+          </Alert>
+        ) : null}
+        {isSuccess ? (
+          <Alert severity="success" variant="outlined">
+            Data updated successfully!
+          </Alert>
+        ) : null}
+        {message ? (
+          <Alert variant="outlined" severity="error">
+            {message}
+          </Alert>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default ProfileEditBlock;
--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -1,24 +1,184 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import s from './ProfilePage.module.scss';
 import clsx from 'clsx';
+
 import { Typography } from '../../components/UI/Typography';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../app/store';
-import { useNavigate } from 'react-router-dom';
+import ProfileEditBlock from './ProfileEditBlock/index';
+import EmailInput from '../../components/UI/FormFields/EmailInput';
+import SimpleInput from '../../components/UI/FormFields/SimpleInput';
+import BirthDateInput from '../../components/UI/FormFields/BirthDateInput';
+import Button from '../../components/UI/Button';
+import { validateEmail } from '../../utils/validators';
+import { CustomerData } from '../../types/types';
+
 import FormTheme from '../../themes/FormTheme';
 import { ThemeProvider } from '@mui/material/styles';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Resolver, useForm, UseFormProps } from 'react-hook-form';
+
+import { RootState, AppDispatch } from '../../app/store';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  setIsDisabledFirstName,
+  setIsDisabledLastName,
+  setIsDisabledDateOfBirth,
+  setDisabledAllFields,
+  setIsDisabledEmail,
+} from '../../app/slices/profile';
+import { clearMessage } from '../../app/slices/message';
+import { getUser, updateUser } from '../../app/slices/auth';
 
 const ProfilePage: React.FC = () => {
+  const [cancelSubmit, setCancelSubmit] = useState(false);
+  const [emailError, setEmailError] = useState('');
+  const [formError, setFormError] = React.useState(false);
+  const [isSuccess, setIsSuccess] = React.useState(false);
+  const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
-  const { isLoggedIn } = useSelector((state: RootState) => state.auth);
+
+  const { isLoggedIn, user } = useSelector((state: RootState) => state.auth);
+  const { message } = useSelector((state: RootState) => state.message);
+  const { isDisabledEmail, isDisabledFirstName, isDisabledLastName, isDisabledDateOfBirth } =
+    useSelector((state: RootState) => state.profile);
+
   useEffect(() => {
     if (!isLoggedIn) {
       navigate('/');
     }
   }, [isLoggedIn, navigate]);
-  const onSubmit = () => {
-    console.log('submit button clicked');
+
+  useEffect(() => {
+    dispatch(getUser());
+    dispatch(clearMessage());
+  }, [dispatch, cancelSubmit]);
+  const currentDate = new Date();
+  const thirteenYearsAgo = new Date(
+    currentDate.getFullYear() - 13,
+    currentDate.getMonth(),
+    currentDate.getDate()
+  );
+
+  const minDate = new Date(1900, 1, 1);
+
+  const schema = yup.object().shape({
+    email: isDisabledEmail
+      ? yup.string()
+      : yup
+          .string()
+          .required('Email is required.')
+          .email('Invalid email (e.g., example@example.com)')
+          .matches(/^[^@]+@[^.]+\..+$/, 'Email should contain a dot in the domain')
+          .test('custom-email-validation', `${emailError}`, (value) => {
+            return validateEmail(value as string, setEmailError);
+          })
+          .test('not-default-email', 'Email should not be the previous value', (value) => {
+            if (user) {
+              return value !== user.email;
+            }
+          }),
+
+    firstName: isDisabledFirstName
+      ? yup.string()
+      : yup
+          .string()
+          .required('First name is required.')
+          .matches(/^[a-zA-Z]+$/, 'First name cannot contain special characters or numbers.'),
+
+    lastName: isDisabledLastName
+      ? yup.string()
+      : yup
+          .string()
+          .required('Last name is required.')
+          .matches(/^[a-zA-Z]+$/, 'Last name cannot contain special characters or numbers.'),
+
+    dateOfBirth: isDisabledDateOfBirth
+      ? yup.string()
+      : yup
+          .date()
+          .max(thirteenYearsAgo)
+          .min(minDate)
+          .typeError('Please enter a valid date.')
+          .required('Date is required.'),
+  });
+
+  const {
+    control,
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<CustomerData>({
+    resolver: yupResolver(schema) as unknown as Resolver<CustomerData>,
+    mode: 'onChange',
+  } as UseFormProps<CustomerData>);
+
+  const onSubmit = (data: CustomerData) => {
+    if (user) {
+      dispatch(updateUser(data))
+        .then((response) => {
+          if (response.meta.requestStatus === 'rejected') {
+            setIsSuccess(false);
+            reset({ email: user ? user.email : '' });
+          } else setIsSuccess(true);
+        })
+        .then(() => {
+          dispatch(getUser());
+        })
+        .then(() => {
+          setTimeout(() => {
+            dispatch(clearMessage());
+            setIsSuccess(false);
+            dispatch(setDisabledAllFields());
+          }, 4000);
+        });
+    }
   };
+
+  const onClickSubmit = () => {
+    setFormError(false);
+    if (errors.email || errors.firstName || errors.lastName || errors.dateOfBirth) {
+      setFormError(true);
+    }
+  };
+  const onClickCancel = () => {
+    dispatch(setDisabledAllFields());
+    setCancelSubmit(!cancelSubmit);
+    setIsSuccess(false);
+    dispatch(clearMessage());
+    reset();
+  };
+
+  const handleEmailClick = () => {
+    dispatch(setIsDisabledEmail());
+    if (!isDisabledEmail) {
+      reset({ email: user ? user.email : '' });
+    }
+  };
+
+  const handleFirstNameClick = () => {
+    dispatch(setIsDisabledFirstName());
+    if (!isDisabledFirstName) {
+      reset({ firstName: user ? user.firstName : '' });
+    }
+  };
+
+  const handleLastNameClick = () => {
+    dispatch(setIsDisabledLastName());
+    if (!isDisabledLastName) {
+      reset({ lastName: user ? user.lastName : '' });
+    }
+  };
+  const handleDateOfBirthClick = () => {
+    dispatch(setIsDisabledDateOfBirth());
+    if (!isDisabledDateOfBirth) {
+      reset({
+        dateOfBirth: user && user.dateOfBirth ? new Date(user.dateOfBirth) : undefined,
+      });
+    }
+  };
+
   return (
     <main>
       <div className={s.container}>
@@ -30,14 +190,46 @@ const ProfilePage: React.FC = () => {
         </div>
       </div>
       <ThemeProvider theme={FormTheme}>
-        <form onSubmit={onSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className={clsx(s.elements__flow)}>
             <div className={clsx(s.form__element, s.form__element_left)}>
               <Typography variant="h2" className={s.form__title_size}>
                 1. Account Info
               </Typography>
             </div>
-            <div className={clsx(s.form__element, s.form__element_flow)}></div>
+            <div className={clsx(s.form__element, s.form__element_flow)}>
+              <EmailInput
+                register={register}
+                errors={errors}
+                defaultValue={user ? user.email : ''}
+                isEditField={true}
+                isDisabled={isDisabledEmail}
+                switchEditModeField={handleEmailClick}
+              />
+              <Button
+                full={true}
+                type="button"
+                variant="secondary"
+                onClick={() => {
+                  const path = '/password';
+                  navigate(path);
+                }}
+                className={s.change_password_btn}
+              >
+                CHANGE PASSWORD
+              </Button>
+              {!isDisabledEmail ? (
+                <div>
+                  <ProfileEditBlock
+                    onClickSubmit={onClickSubmit}
+                    onClickCancel={onClickCancel}
+                    formError={formError}
+                    isSuccess={isSuccess}
+                    message={message}
+                  />
+                </div>
+              ) : null}
+            </div>
           </div>
           <div className={clsx(s.elements__flow)}>
             <div className={clsx(s.form__element, s.form__element_left)}>
@@ -45,7 +237,55 @@ const ProfilePage: React.FC = () => {
                 2. User Info
               </Typography>
             </div>
-            <div className={clsx(s.form__element, s.form__element_flow)}></div>
+            <div className={clsx(s.form__element, s.form__element_flow)}>
+              <SimpleInput
+                register={register}
+                errors={errors}
+                err={errors.firstName}
+                errMessage={errors.firstName?.message}
+                name={'firstName'}
+                label="First name *"
+                id="firstName-input"
+                defaultValue={user ? user.firstName : ''}
+                isEditField={true}
+                isDisabled={isDisabledFirstName}
+                switchEditModeField={handleFirstNameClick}
+              />
+              <SimpleInput
+                register={register}
+                errors={errors}
+                err={errors.lastName}
+                errMessage={errors.lastName?.message}
+                name={'lastName'}
+                label="Last name *"
+                id="lastName-input"
+                defaultValue={user ? user.lastName : ''}
+                isEditField={true}
+                isDisabled={isDisabledLastName}
+                switchEditModeField={handleLastNameClick}
+              />
+              <BirthDateInput
+                register={register}
+                errors={errors}
+                control={control}
+                reset={reset}
+                defaultValue={user && user.dateOfBirth ? new Date(user.dateOfBirth) : null}
+                isEditField={true}
+                isDisabled={isDisabledDateOfBirth}
+                switchEditModeField={handleDateOfBirthClick}
+              />
+              {!isDisabledFirstName || !isDisabledLastName ? (
+                <div>
+                  <ProfileEditBlock
+                    onClickSubmit={onClickSubmit}
+                    onClickCancel={onClickCancel}
+                    formError={formError}
+                    isSuccess={isSuccess}
+                    message={message}
+                  />
+                </div>
+              ) : null}
+            </div>
           </div>
         </form>
       </ThemeProvider>
