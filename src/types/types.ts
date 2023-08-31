df9b33ec44aa721fea91d89fdfc3fdd1a3601a1b
Mialiokhin <a.mialiokhin@gmail.com>
2023-08-31 22:51:41 +0300

    feat (rs-flowershop-60) add types for profilePage

--- src/types/types.ts
+++ src/types/types.ts
@@ -15,6 +15,11 @@ export interface CustomerData {
   billingPostalCode: string;
   billingCountry: string;
   billingDefaultAddress: boolean;
+  shippingAddressIds?: string[];
+  billingAddressIds?: string[];
+  defaultBillingAddressId?: string;
+  defaultShippingAddressId?: string;
+  addresses?: Address[] | [];
 }
 
 export type Address = {
@@ -22,6 +27,25 @@ export type Address = {
   postalCode: string;
   country: string;
   city: string;
+};
+
+export interface ProfileForm extends Address {
+  shippingBillingAddress?: boolean;
+  billingShippingAddress?: boolean;
+  shippingDefaultAddress?: boolean;
+  billingDefaultAddress?: boolean;
+  id: string;
+}
+
+export interface ProfileAddress extends Address {
+  id: string;
+}
+
+export type AddressAction = 'shipping' | 'billing' | 'no_assigned';
+
+export type DelAddress = {
+  id: string;
+  action: string;
 };
 
 export type RequestPayload = {
@@ -36,3 +60,33 @@ export type RequestPayload = {
   shippingAddresses?: number[];
   billingAddresses?: number[];
 };
+
+export interface User {
+  id: string;
+  version: number;
+  createdAt: string;
+  lastModifiedAt: string;
+  lastModifiedBy: {
+    clientId: string;
+    isPlatformClient: boolean;
+  };
+  createdBy: {
+    clientId: string;
+    isPlatformClient: boolean;
+  };
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  addresses: ProfileAddress[] | [];
+  authenticationMode: string;
+  billingAddressIds?: string[];
+  shippingAddressIds?: string[];
+  isEmailVerified: boolean;
+  stores: string[];
+  defaultBillingAddressId?: string | null;
+  defaultShippingAddressId?: string | null;
+  dateOfBirth: string;
+  lastMessageSequenceNumber?: number;
+  versionModifiedAt: string;
+}
