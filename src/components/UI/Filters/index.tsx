80f305e41133d3529260aa5cb804332ca9d2fb33
Gleb Elenev <g.elenev@gmail.com>
2023-09-03 16:28:06 +0300

    feat (rs-flowershop-50,53) add filters,breadcrumbs

--- src/components/UI/Filters/index.tsx
+++ src/components/UI/Filters/index.tsx
@@ -1,11 +1,16 @@
-import clsx from 'clsx';
 import SortingBlock from '../SortingBlock';
+import FilterBlock from '../FilterBlock';
 import s from './Filters.module.scss';
+import CatalogBreadcrumbs from '../CatalogBreadcrumbs';
 
 function Filters({ className }: JSX.IntrinsicElements['div']) {
   return (
-    <div className={clsx(s.block, className)}>
-      <SortingBlock />
+    <div className={className}>
+      <div className={s.block}>
+        <CatalogBreadcrumbs />
+        <SortingBlock />
+        <FilterBlock />
+      </div>
     </div>
   );
 }
